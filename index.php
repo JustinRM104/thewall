1dca6e184284f87a5848e55182391f79731b8020
Justin Rodriguez <rmjustin02@gmail.com>
2020-02-26 10:15:26 +0100

    Aanmaak van index.php

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="nl" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="author" content="Justin Rodriguez Montoya, Mike Yang">
+    <meta name="keywords" content="feed, fashion, kleding, streetware, jongvolwassen, broek, trui, verhaal, sociaal">
+    <meta name="description" content="De fashion feed van Nederland!">
+    <title></title>
+  </head>
+  <body>
+
+  </body>
+</html>
