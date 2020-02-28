9bb6d4547410cae6520f445c9445b9418f0c21a8
Justin Rodriguez <rmjustin02@gmail.com>
2020-02-28 10:56:44 +0100

    Nieuwe index en css.

--- index.php
+++ index.php
@@ -6,9 +6,34 @@
     <meta name="author" content="Justin Rodriguez Montoya, Mike Yang">
     <meta name="keywords" content="feed, fashion, kleding, streetware, jongvolwassen, broek, trui, verhaal, sociaal">
     <meta name="description" content="">
-    <title></title>
+
+    <link rel="stylesheet" href="master.css">
+    <link rel="shortcut icon" href="img/logo.png">
+    <title>FashionSwipe | Feed</title>
+
+    <script src="https://kit.fontawesome.com/7023acb312.js" crossorigin="anonymous"></script>
   </head>
   <body>
+    <div class="feed">
+      <div class="top">
+        <h2><span class="fas fa-comments"></span> Feed</h2>
+      </div>
+      <form class="messageBox" action="index.html" method="post">
+        <input type="title" name="title" value="" placeholder="Titel" class="title" required>
+        <textarea name="message" rows="8" cols="80" placeholder="Plaats een bericht" required></textarea>
+        <div class="functions">
+          <input type="file" class="upload" name="filename" accept=".jpg, .jpeg, .png">
+        </div>
+        <a href="#temp"><span class="far fa-paper-plane send"></span> Plaats</a>
+      </form>
+      <div class="posts">
+        <section>
+          <h2><span>Gebruiker</span> - Titel</h2>
+          <p>hallo</p>
+          <img src="https://cdn.shopify.com/s/files/1/0039/4225/2662/products/product-image-438717472.jpg?v=1538384920" alt="">
 
+        </section>
+      </div>
+    </div>
   </body>
 </html>
